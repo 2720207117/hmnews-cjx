18714de8a7e74ce4422dcefe9fa885d5aae2080a
CJX2720207117 <2720207117@qq.com>
2019-08-01 19:27:25 +0800

    项目-登录-页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>login</div>
+  <div class="login">
+    <form action="/">
+      <!-- 导航栏 -->
+      <van-nav-bar title="登录" type="primary" />
+
+      <!-- 表单 -->
+      <van-cell-group>
+        <!-- required:  在输入框前会显示红色*，表示此处必须填 -->
+        <!-- clearable: ' x '符，清除输入框中的内容 -->
+        <van-field
+          v-model="user.mobile"
+          label="手机号"
+          placeholder="请输入手机号"
+          required
+          clearable
+        />
+
+        <van-field
+          v-model="user.code"
+          type="text"
+          label="验证码"
+          placeholder="请输入验证码"
+          required
+        />
+        <van-button type="info">登录</van-button>
+      </van-cell-group>
+    </form>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: null,
+        code: null
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login{
+  text-align: center;
+  .van-nav-bar {  // 导航栏大小 及 背景颜色
+    height: 128px;
+    line-height: 128px;
+    background-color: #3296fa;
+    .van-nav-bar__title { // 导航栏字体颜色
+      color: #fff;
+    }
+  }
+}
+.van-button { // 登录按钮大小 及 位置
+  width: 694px;
+  margin-top: 53px;
+}
 </style>
